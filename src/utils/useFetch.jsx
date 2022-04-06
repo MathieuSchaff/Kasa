a749e563ad9e86457b0b398f13248a7ee778c6a7
Schaff <schaff.mathieu93@gmail.com>
2022-04-07 01:54:45 +0200

    add config airbnb + a11y

--- src/utils/useFetch.jsx
+++ src/utils/useFetch.jsx
@@ -1,7 +1,3 @@
-import './App.css'
-import React from 'react'
-import {useEffect, useState} from 'react'
-import data from './data/data.json'
 // export function useFetch(url) {
 //   const [data, setData] = useState({})
 
@@ -40,14 +36,3 @@ import data from './data/data.json'
 
 //   return {isLoading, data, error}
 // }
-function App() {
-  const [data, setData] = useState(data)
-
-  useEffect(() => {
-    console.log(data)
-  }, [])
-
-  return <div className="App">{data && <div> aoeo </div>}</div>
-}
-
-export default App
