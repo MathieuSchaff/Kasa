8b36d9952966db52f9c886bdd17d6d18d8ff622a
Schaff <schaff.mathieu93@gmail.com>
2022-04-07 01:40:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
+import {useEffect, useState} from 'react'
+import data from './data/data.json'
+// export function useFetch(url) {
+//   const [data, setData] = useState({})
 
+//   const [isLoading, setLoading] = useState(true)
+
+//   const [error, setError] = useState(false)
+
+//   useEffect(() => {
+//     if (!url) return
+
+//     setLoading(true)
+
+//     async function fetchData() {
+//       try {
+//         const response = await fetch(url, {
+//           headers: {
+//             'Content-Type': 'application/json',
+//             Accept: 'application/json',
+//           },
+//         })
+
+//         const data = await response.json()
+//         console.log(data)
+//         setData(data)
+//       } catch (err) {
+//         console.log(err)
+
+//         setError(true)
+//       } finally {
+//         setLoading(false)
+//       }
+//     }
+
+//     fetchData()
+//   }, [url])
+
+//   return {isLoading, data, error}
+// }
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [data, setData] = useState({})
+
+  // const [isLoading, setLoading] = useState(true)
+
+  // const [error, setError] = useState(false)
+
+  // // setLoading(true)
+
+  // const fetchData = async () => {
+  //   const data = await fetch('./data/data.json', {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Accept: 'application/json',
+  //     },
+  //   })
+  //   // .then(messages => {
+  //   //   console.log(messages)
+  //   // })
+
+  //   //   const data = await response.json()
+
+  //   //   setData(data)
+  //   // } catch (err) {
+  //   //   console.log(err)
+
+  //   //   setError(true)
+  //   // } finally {
+  //   //   setLoading(false)
+  //   // }
+  //   setData(await data.json())
+  // }
+
+  useEffect(() => {
+    console.log(data)
+  }, [])
+
+  // const {data, isLoading, error} = useFetch(`../data/data.json`)
+  // useEffect(() => {
+  //   console.log(data)
+  // }, [data])
+  return <div className="App">{data && <div> aoeo </div>}</div>
 }
 
-export default App;
+export default App
