64ee925290f0fe207226d130cf9fbe145ae61ca7
Schaff <schaff.mathieu93@gmail.com>
2022-04-07 02:18:58 +0200

    basic style added in sass

--- src/App.js
+++ src/App.js
@@ -2,50 +2,11 @@ import './App.css'
 import React from 'react'
 import {useEffect, useState} from 'react'
 import data from './data/data.json'
-// export function useFetch(url) {
-//   const [data, setData] = useState({})
 
-//   const [isLoading, setLoading] = useState(true)
-
-//   const [error, setError] = useState(false)
-
-//   useEffect(() => {
-//     if (!url) return
-
-//     setLoading(true)
-
-//     async function fetchData() {
-//       try {
-//         const response = await fetch(url, {
-//           headers: {
-//             'Content-Type': 'application/json',
-//             Accept: 'application/json',
-//           },
-//         })
-
-//         const data = await response.json()
-//         console.log(data)
-//         setData(data)
-//       } catch (err) {
-//         console.log(err)
-
-//         setError(true)
-//       } finally {
-//         setLoading(false)
-//       }
-//     }
-
-//     fetchData()
-//   }, [url])
-
-//   return {isLoading, data, error}
-// }
 function App() {
-  const [data, setData] = useState(data)
-
   useEffect(() => {
     console.log(data)
-  }, [])
+  }, [data])
 
   return <div className="App">{data && <div> aoeo </div>}</div>
 }
